e5ff4ed550dd64737c696662853cd7cbafcf1e96
miiinhhh <minhming5542@gmail.com>
2026-09-17 20:00:40 +0700

    Create AccessibilityService

---
+++ app/src/main/java/com/example/autoclick/service/AutoClickService.java
@@ -0,0 +1,15 @@
+package com.example.autoclick.service;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class AutoClickService extends AccessibilityService {
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+    }
+
+    @Override
+    public void onInterrupt() {
+    }
+}
